45eae338dddb3685edb65b19b1094c485e6c5493
Łukasz Czernik <lukasz.czernik@gmail.com>
2022-06-09 21:42:45 +0200

    Update and rename index.py to SmartMelkom_1.0.py

---
+++ SmartMelkom_1.0.py
@@ -0,0 +1,42 @@
+# Main
+for x in 10.78.x.x:
+  if ping x:
+    run instance
+    
+    
+def find_open(ip: str):
+  # [123, 91, x, x]
+  
+  [[
+    [out, in_] for in_ in range(1,255)
+    ] for out in range(1,255)]
+  
+  split_ip = ip.split(".")
+  
+  test = [None, None, None, None]
+  for i, slot in enumerate(ip_split):
+    if slot is int:
+      test[i] = slot
+    else:
+      for x in range(255)
+        find_open(test)
+      
+  
+  
+  
+def report_existing(ip: str):
+  # message to parent
+  # if parrent doas not exist 
+  # take controll
+  # message other childs
+  pass
+
+
+
+def run_mining():
+  pass
+
+
+  
+
+# Instance
